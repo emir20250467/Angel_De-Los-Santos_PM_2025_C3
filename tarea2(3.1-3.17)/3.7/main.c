9a57d6d90ede7ab8bcd399cd6c4f5756d26e6da6
ángel emir de los santos cordero <emir567808@gmail.com>
2025-09-26 00:08:34 -0400

    Add files via upload

---
+++ tarea2(3.1-3.17)/3.7/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+    int I,N;
+    float LAN,SLA = 0;
+    do
+    {
+        printf("Ingrese el numero de lanzamiento:\t");
+        scanf("%d",&N);
+    }
+    while(N < 1 || N > 11);
+    for(I=1; I<=N; I++)
+    {
+        printf("\nIngrese el lanzamiento %d: ",I);
+        scanf("%f",&LAN);
+        SLA = SLA + LAN;
+    }
+    SLA = SLA / N;
+    printf("\nEL promedio de lanzamiento es: %.2f",SLA);
+}
